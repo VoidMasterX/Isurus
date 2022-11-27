ec6318021369e3f95f58c7bf00b57e7245db3ff9
Siper <71610591+VoidMasterX@users.noreply.github.com>
2022-11-26 19:56:57 -0600

    Update esp.lua

--- libraries/esp.lua
+++ libraries/esp.lua
@@ -29,6 +29,39 @@ local ccWorldToViewportPoint = currentCamera.WorldToViewportPoint;
 local pointToObjectSpace = cframeNew().PointToObjectSpace;
 
 -- support functions
+local function format(tbl, tblMerge, format)
+    local formatted = tbl;
+    local formattedMerge = tblMerge;
+
+    if (format) then
+        for index, value in next, tbl do
+            if (typeof(index) == "string") then
+                formatted[index:lower()] = value;
+            else
+                formatted[index] = value;
+            end
+        end
+
+        for index, value in next, tblMerge do
+            if (typeof(index) == "string") then
+                formattedMerge[index:lower()] = value;
+            else
+                formattedMerge[index] = value;
+            end
+        end
+    end
+
+    if (tblMerge) then
+        for index, _ in next, formatted do
+            if (formattedMerge[index] ~= nil and typeof(formattedMerge[index]) == typeof(formatted[index])) then
+                formatted[index] = formattedMerge[index];
+            end
+        end
+    end
+
+    return formatted;
+end
+
 local function worldToViewportPoint(position)
     if (isSynV3) then
         local screenPosition = worldtoscreen({ position })[1];
@@ -69,14 +102,16 @@ end
 
 -- main module
 local library = {
+    _initialized = false,
     _connections = {},
     _espCache = {},
     _chamsCache = {},
+    _objectCache = {},
     _screenGui = create("ScreenGui", {
         Parent = coreGui,
     }),
     settings = {
-        enabled = true,
+        enabled = false,
         visibleOnly = false,
         teamCheck = false,
         boxStaticWidth = 4,
@@ -90,18 +125,18 @@ local library = {
         chamsInlineTransparency = 0,
         chamsOutlineColor = color3New(),
         chamsOutlineTransparency = 0,
-        names = true,
+        names = false,
         nameColor = color3New(1, 1, 1),
         teams = false,
         teamColor = color3New(1, 1, 1),
         teamUseTeamColor = false,
-        boxes = true,
+        boxes = false,
         boxColor = color3New(1, 0, 0),
-        boxType = "Static",
-        boxFill = true,
+        boxType = "Dynamic",
+        boxFill = false,
         boxFillColor = color3New(1, 0, 0),
         boxFillTransparency = 0.5,
-        healthbar = true,
+        healthbar = false,
         healthbarColor = color3New(0, 1, 0.4),
         healthbarSize = 1,
         healthtext = false,
@@ -169,17 +204,17 @@ function library._getScaleFactor(fov, depth)
     return 1 / (lastScale * depth) * 1000;
 end
 
-function library._getBoxSize(character)
-    if (library.settings.boxType == "Static" or not isA(character, "Model")) then
+function library._getBoxSize(model)
+    if (library.settings.boxType == "Static" or not isA(model, "Model")) then
         return vector2New(library.settings.boxStaticWidth, library.settings.boxStaticHeight);
     end
 
-    local _, size = character:GetBoundingBox();
+    local _, size = model:GetBoundingBox();
     return vector2New(mathClamp(size.X, 0, library.settings.maxBoxWidth), mathClamp(size.Y, 0, library.settings.maxBoxHeight));
 end
 
-function library._getBoxData(character, depth)
-    local size = library._getBoxSize(character);
+function library._getBoxData(model, depth)
+    local size = (typeof(model) == "Vector2" or typeof(model) == "Vector3") and model or library._getBoxSize(model);
     local scaleFactor = library._getScaleFactor(currentCamera.FieldOfView, depth);
     return mathFloor(size.X * scaleFactor), mathFloor(size.Y * scaleFactor);
 end
@@ -306,7 +341,112 @@ function library._removeChams(player)
     end
 end
 
+function library._addObject(object, root, options)
+    if (library._objectCache[object]) then
+        return
+    end
+
+    local font = isSynV3 and 1 or 2;
+
+    local objects = {
+        name = create("Text", {
+            Color = options.nameColor,
+            Text = options.name,
+            Size = 13,
+            Center = true,
+            Outline = true,
+            OutlineColor = color3New(),
+            Font = font
+        }),
+        info = create("Text", {
+            Color = options.infoColor,
+            Size = 13,
+            Center = true,
+            Outline = true,
+            OutlineColor = color3New(),
+            Font = font
+        }),
+        boxOutline = create("Square", {
+            Color = color3New(),
+            Transparency = 0.5,
+            Thickness = 3,
+            Filled = false
+        }),
+        box = create("Square", {
+            Color = options.boxColor,
+            Thickness = 1,
+            Filled = false
+        }),
+        boxFill = create("Square", {
+            Color = options.boxFillColor,
+            Transparency = options.boxFillTransparency,
+            Thickness = 1,
+            Filled = true
+        }),
+        distance = create("Text", {
+            Color = options.distanceColor,
+            Size = 13,
+            Center = true,
+            Outline = true,
+            OutlineColor = color3New(),
+            Font = font
+        })
+    };
+
+    library._objectCache[object] = {
+        _root = root,
+        _options = options,
+        _objects = objects
+    };
+end
+
+function library._removeObject(object)
+    local cache = library._objectCache[object];
+
+    if (cache) then
+        for index, object in next, cache._objects do
+            object:Remove();
+            cache[index] = nil;
+        end
+
+        library._objectCache[object] = nil;
+    end
+end
+
+function library:AddObject(object, root, options)
+    local defaultOptions = format({
+        enabled = false,
+        limitDistance = false,
+        maxDistance = 250,
+        name = object.Name,
+        names = false,
+        nameColor = color3New(1, 1, 1),
+        boxes = false,
+        boxColor = color3New(1, 0, 0),
+        boxFill = false,
+        boxFillColor = color3New(1, 0, 0),
+        boxFillTransparency = 0.5,
+        text = "",
+        info = false,
+        infoColor = color3New(1, 1, 1),
+        distance = false,
+        distanceColor = color3New(1, 1, 1)
+    }, options, true);
+
+    if (isA(object, "Model") or isA(object, "BasePart")) then
+        self._addObject(object, root, defaultOptions);
+    end
+
+    return defaultOptions;
+end
+
+function library:RemoveObject(object)
+    self._removeObject(object);
+end
+
 function library:Load()
+    self._initialized = true;
+
     for _, player in next, players:GetPlayers() do
         self._addEsp(player);
         self._addChams(player);
@@ -320,6 +460,93 @@ function library:Load()
     self:AddConnection(players.PlayerRemoving, function(player)
         self._removeEsp(player);
         self._removeChams(player);
+    end);
+
+    self:AddConnection(workspace.DescendantRemoving, function(object)
+        self._removeObject(object);
+    end);
+
+    self:AddConnection(runService.Heartbeat, function()
+        for object, cache in next, self._objectCache do
+            local options, objects, root = cache._options, cache._objects, cache._root;
+
+            local cameraCFrame = currentCamera.CFrame;
+            local cameraPosition, rootPosition = cameraCFrame.Position, root.Position;
+            local magnitude = (cameraPosition - rootPosition).Magnitude;
+
+            local enabled = options.enabled;
+
+            if (options.limitDistance and magnitude > options.maxDistance) then
+                enabled = false;
+            end
+
+            if (options and objects and root and enabled) then
+                local rootPosition, onScreen, depth = worldToViewportPoint(rootPosition);
+
+                local x, y = rootPosition.X, rootPosition.Y;
+                local width, height = self._getBoxData(isA(object, "Model") and select(2, object:GetBoundingBox()) or object.Size, depth);
+                local boxSize = vector2New(width, height);
+                local boxPosition = vector2New(mathFloor(x - width * 0.5), mathFloor(y - height * 0.5));
+
+                objects.name.Visible = onScreen and options.names;
+                objects.name.Position = vector2New(x, boxPosition.Y - objects.name.TextBounds.Y - 2);
+
+                objects.info.Visible = onScreen and options.info;
+                objects.info.Text = options.text;
+                objects.info.Color = options.infoColor;
+                objects.info.Position = vector2New(x + width * 0.5 + objects.info.TextBounds.X * 0.5 + 2, boxPosition.Y - 2);
+
+                objects.box.Visible = onScreen and options.boxes;
+                objects.box.Color = options.boxColor;
+                objects.box.Size = boxSize;
+                objects.box.Position = boxPosition;
+
+                objects.boxOutline.Visible = objects.box.Visible;
+                objects.boxOutline.Size = boxSize;
+                objects.boxOutline.Position = boxPosition;
+
+                objects.boxFill.Visible = onScreen and options.boxFill;
+                objects.boxFill.Color = options.boxFillColor;
+                objects.boxFill.Transparency = options.boxFillTransparency;
+                objects.boxFill.Size = boxSize;
+                objects.boxFill.Position = boxPosition;
+
+                objects.distance.Visible = onScreen and options.distance;
+                objects.distance.Text = mathFloor(magnitude) .. " Studs";
+                objects.distance.Color = options.distanceColor;
+                objects.distance.Position = vector2New(x, boxPosition.Y + height);
+            else
+                for _, object in next, objects do
+                    object.Visible = false;
+                end
+            end
+        end
+    end);
+
+    self:AddConnection(runService.Heartbeat, function()
+        for player, highlight in next, self._chamsCache do
+            local team = self._getTeam(player);
+            local character = self._getCharacter(player);
+
+            if (character) then
+                local enabled = self.settings.chams;
+
+                if (self.settings.teamCheck and team == self._getTeam(localPlayer)) then
+                    enabled = false
+                end
+
+                highlight.Enabled = enabled;
+                highlight.Adornee = character;
+                highlight.DepthMode = depthMode[self.settings.chamsDepthMode];
+                highlight.FillColor = self.settings.chamsInlineColor;
+                highlight.FillTransparency = self.settings.chamsInlineTransparency;
+                highlight.OutlineColor = self.settings.chamsOutlineColor;
+                highlight.OutlineTransparency = self.settings.chamsOutlineTransparency;
+            else
+                highlight.Enabled = false;
+                highlight.Adornee = nil;
+            end
+        end
     end);
 
     self:AddConnection(runService.Heartbeat, function()
@@ -426,35 +653,10 @@ function library:Load()
             end
         end
     end);
-
-    self:AddConnection(runService.Heartbeat, function()
-        for player, highlight in next, self._chamsCache do
-            local team = self._getTeam(player);
-            local character = self._getCharacter(player);
-
-            if (character) then
-                local enabled = self.settings.chams;
-
-                if (self.settings.teamCheck and team == self._getTeam(localPlayer)) then
-                    enabled = false
-                end
-
-                highlight.Enabled = enabled;
-                highlight.Adornee = character;
-                highlight.DepthMode = depthMode[self.settings.chamsDepthMode];
-                highlight.FillColor = self.settings.chamsInlineColor;
-                highlight.FillTransparency = self.settings.chamsInlineTransparency;
-                highlight.OutlineColor = self.settings.chamsOutlineColor;
-                highlight.OutlineTransparency = self.settings.chamsOutlineTransparency;
-            else
-                highlight.Enabled = false;
-                highlight.Adornee = nil;
-            end
-        end
-    end);
 end
 
 function library:Unload()
+    self._initialized = false;
     self._screenGui:Destroy();
 
     for index, connection in next, self._connections do
@@ -465,6 +667,10 @@ function library:Unload()
     for _, player in next, players:GetPlayers() do
         self._removeEsp(player);
         self._removeChams(player);
+    end
+
+    for object, _ in next, self._objectCache do
+        self._removeObject(object);
     end
 end
 
