5cb715df4bab7286e449be1e7958f4c1e53839f6
Crcoli737 <cray.c.litton@gmail.com>
2022-12-11 01:36:09 -0500

    Optimize script

--- script.lua
+++ script.lua
@@ -1,6 +1,4 @@
--- start
 local version = "1.0";
-
 local username = "VoidMasterX";
 local repository = "Isurus";
 local baseUrl = "https://raw.githubusercontent.com/" .. username .. "/" .. repository .. "/main/";
@@ -13,18 +11,15 @@ local function loadLibrary(library)
     return loadstring(import("libraries/" .. library .. ".lua"))();
 end
 
--- libraries
-local library = loadLibrary("ui");
-local espLibrary = loadLibrary("esp");
+local libraries = {
+    ui = loadLibrary("ui"),
+    esp = loadLibrary("esp"),
+};
 
--- modules
 local sharedRequire = getrenv().shared.require;
 local replicationInterface = sharedRequire("ReplicationInterface");
 
--- framework
-local framework = {}; do
-    
-end
+local framework = {};
 
 -- combat
 do
@@ -33,6 +28,8 @@ end
 
 -- visuals
 do
+    local espLibrary = libraries.esp;
+
     function espLibrary._getCharacter(player)
         local entry = replicationInterface.getEntry(player);
         local thirdPersonObject = entry and entry:getThirdPersonObject();
@@ -71,6 +68,8 @@ end
 
 -- ui
 do
+    local library = libraries.ui;
+
     library.font = (worldtoscreen ~= nil) and 1 or 2;
 
     local window = library:load({
@@ -88,118 +87,67 @@ do
                 local showFOV = aimbot:toggle({ name = "Show Field Of View", flag = "legitbot_aimbot_showfov" });
                 showFOV:colorpicker({ default = Color3.new(1, 1, 1), flag = "legitbot_aimbot_fovcolor" });
                 showFOV:slider({ name = "Field Of View", min = 0, max = 20, default = 10, flag = "legitbot_aimbot_fov" });
-                aimbot:slider({ name = "Smoothing", min = 3, max = 100, default = 10, flag = "legitbot_aimbot_smoothing" });
-            end
-        end
-
-        local visuals = window:tab("Visuals"); do
-            local esp = visuals:section({ name = "Esp", side = "left" }); do
-                esp:toggle({ name = "Enabled", flag = "visuals_esp_enabled", callback = function(state)
-                    espLibrary.settings.enabled = state;
-                end });
-                esp:toggle({ name = "Names", flag = "visuals_esp_names", callback = function(state)
-                    espLibrary.settings.names = state;
-                end });
-                esp:toggle({ name = "Team", flag = "visuals_esp_team", callback = function(state)
-                    espLibrary.settings.teams = state;
-                end });
-                local boxes = esp:toggle({ name = "Boxes", flag = "visuals_esp_boxes", callback = function(state)
-                    espLibrary.settings.boxes = state;
-                end });
-                boxes:colorpicker({ default = Color3.new(1, 1, 1), flag = "visuals_esp_boxcolor", callback = function(color)
-                    espLibrary.settings.boxColor = color;
-                end });
-                boxes:dropdown({ content = { "Static", "Dynamic" }, default = "Static", flag = "visuals_esp_boxtype", callback = function(selected)
-                    espLibrary.settings.boxType = selected;
-                end })
-                esp:toggle({ name = "Box Fill", flag = "visuals_esp_boxfill", callback = function(state)
-                    espLibrary.settings.boxFill = state;
-                end });
-                esp:toggle({ name = "Healthbar", flag = "visuals_esp_healthbar", callback = function(state)
-                    espLibrary.settings.healthbar = state;
-                end });
-                esp:toggle({ name = "Health Text", flag = "visuals_esp_healthtext", callback = function(state)
-                    espLibrary.settings.healthtext = state;
-                end });
-                esp:toggle({ name = "Distance", flag = "visuals_esp_distance", callback = function(state)
-                    espLibrary.settings.distance = state;
-                end });
-                esp:toggle({ name = "Weapon", flag = "visuals_esp_weapon", callback = function(state)
-                    espLibrary.settings.weapon = state;
-                end });
-                esp:toggle({ name = "Out Of View Arrows", flag = "visuals_esp_oofarrows", callback = function(state)
-                    espLibrary.settings.oofArrows = state;
-                end });
-            end
-
-            local settings = visuals:section({ name = "Settings", side = "left" }); do
-                
-            end
-        end
-
-        local settings = window:tab("Settings"); do
-            local configuration = settings:section({ name = "Configuration", side = "left" }); do
-                local function getConfigs()
-                    local list = {};
-                    local configs = library:GetConfigs();
-
-                    for _, config in next, configs do
-                        list[#list+1] = config:gsub(library.folder .. "\\" .. game.PlaceId .. "\\", "");
-                    end
-
-                    return list;
+                aimbot:slider({
+                    name = "Smoothing", min = 3, max = 100, default = 10, flag = "legitbot_aimbot_smoothing" });
                 end
-
-                local configList = configuration:list({ name = "Configs", content = getConfigs(), scrollable = true, scrollingmax = 11, flag = "Config List" });
-
-                configuration:box({ placeholder = "Config Name", flag = "Config Box" });
-
-                configuration:button({ name = "Refresh Configs", callback = function()
-                    configList:Refresh(getConfigs());
-                end });
-
-                configuration:button({ name = "Create Config", callback = function()
-                    library:SaveConfig(library.flags["Config Box"]);
-                    configList:Refresh(getConfigs());
-                end });
-
-                configuration:button({ name = "Load Config", callback = function()
-                    library:LoadConfig(library.flags["Config List"]);
-                end });
-
-                configuration:button({ name = "Save Config", callback = function()
-                    library:SaveConfig(library.flags["Config List"]);
-                end });
-
-                configuration:button({ name = "Delete Config", callback = function()
-                    library:DeleteConfig(library.flags["Config List"]);
-                    configList:Refresh(getConfigs());
-                end });
-
-                library:ConfigIgnore("Config List");
-                library:ConfigIgnore("Config Box");
             end
-
-            local theme = settings:section({ name = "Theme", side = "right" }); do
-                for option, color in next, library.theme do
-                    theme:colorpicker({ name = option, default = color, flag = option, callback = function(newColor)
-                        library:ChangeThemeOption(option, newColor);
+    
+            local visuals = window:tab("Visuals"); do
+                local esp = visuals:section({ name = "Esp", side = "left" }); do
+                    esp:toggle({ name = "Enabled", flag = "visuals_esp_enabled", callback = function(state)
+                        espLibrary.settings.enabled = state;
+                    end });
+                    esp:toggle({ name = "Names", flag = "visuals_esp_names", callback = function(state)
+                        espLibrary.settings.names = state;
+                    end });
+                    esp:toggle({ name = "Team", flag = "visuals_esp_team", callback = function(state)
+                        espLibrary.settings.teams = state;
+                    end });
+                    local boxes = esp:toggle({ name = "Boxes", flag = "visuals_esp_boxes", callback = function(state)
+                        espLibrary.settings.boxes = state;
+                    end });
+                    boxes:colorpicker({ default = Color3.new(1, 1, 1), flag = "visuals_esp_boxcolor" });
+                    esp:toggle({ name = "Health", flag = "visuals_esp_health", callback = function(state)
+                        espLibrary.settings.health = state;
+                    end });
+                    esp:toggle({ name = "Weapons", flag = "visuals_esp_weapons", callback = function(state)
+                        espLibrary.settings.weapons = state;
+                    end });
+                end
+    
+                local chams = visuals:section({ name = "Chams", side = "right" }); do
+                    chams:toggle({ name = "Enabled", flag = "visuals_chams_enabled", callback = function(state)
+                        espLibrary.settings.chams = state;
+                    end });
+                    chams:colorpicker({ default = Color3.new(1, 1, 1), flag = "visuals_chams_color" });
+                    chams:toggle({ name = "Material", flag = "visuals_chams_material", callback = function(state)
+                        espLibrary.settings.chamsMaterial = state;
+                    end });
+                    chams:dropdown({ name = "Material Type", options = { "Plastic", "Neon", "Glow", "Glass", "Crystal", "Gold" }, flag = "visuals_chams_materialtype" });
+                    chams:toggle({ name = "Wireframe", flag = "visuals_chams_wireframe", callback = function(state)
+                        espLibrary.settings.wireframe = state;
                     end });
                 end
             end
-
-            local cheat = settings:section({ name = "Cheat", side = "right" }); do
-                cheat:keybind({ name = "Menu Key", default = library.keybind, flag = "Menu Key", callback = function()
-                    library:Close();
-                end });
-
-                cheat:button({ name = "Unload", callback = function()
-                    espLibrary:Unload();
-                    library:Unload();
-                end });
+    
+            local radar = window:tab("Radar"); do
+                local settings = radar:section({ name = "Settings", side = "left" }); do
+                    settings:toggle({ name = "Enabled", flag = "radar_enabled" });
+                    settings:toggle({ name = "Show Team", flag = "radar_showteam" });
+                    settings:toggle({ name = "Show Enemy", flag = "radar_showenemy" });
+                    settings:toggle({ name = "Show Weapons", flag = "radar_showweapons" });
+                    local size = settings:slider({ name = "Size", min = 200, max = 800, default = 400, flag = "radar_size" });
+                    size:slider({ name = "X Offset", min = 0, max = 800, default = 400, flag = "radar_offset_x" });
+                    size:slider({ name = "Y Offset", min = 0, max = 600, default = 300, flag = "radar_offset_y" });
+                end
+    
+                local colors = radar:section({ name = "Colors", side = "right" }); do
+                    colors:colorpicker({ default = Color3.new(1, 1, 1), flag = "radar_color_ally" });
+                    colors:colorpicker({ default = Color3.new(1, 1, 1), flag = "radar_color_enemy" });
+                    colors:colorpicker({ default = Color3.new(1, 1, 1), flag = "radar_color_weapon" });
+                end
             end
         end
+    
+        window:show();
     end
-
-    library:Close();
-end
